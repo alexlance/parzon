7353e17f9967deb71102425da74253da20c1aaa0
Alex Lance <code@alexlance.com>
2016-04-28 11:56:22 +1000

    Linting

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 """ Wrapper to enable executable entrypoint for program. """
 from setuptools import setup
 
-setup(name='parzon',
-      version='0.7',
-      packages=['parzon'],
-      entry_points={
-          'console_scripts': [
-              'parzon = parzon.__main__:main'
-          ]
-      },
-     )
+setup(name='parzon', version='0.7', packages=['parzon'],
+      entry_points={'console_scripts': ['parzon = parzon.__main__:main']})
