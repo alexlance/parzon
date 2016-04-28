3137caf8f78899f40bc492d032074406de78b3b0
Alex Lance <code@alexlance.com>
2016-04-28 12:19:40 +1000

    Nudge version number

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """ Wrapper to enable executable entrypoint for program. """
 from setuptools import setup
 
-setup(name='parzon', version='0.7', packages=['parzon'],
+setup(name='parzon', version='0.8', packages=['parzon'],
       entry_points={'console_scripts': ['parzon = parzon.__main__:main']})
